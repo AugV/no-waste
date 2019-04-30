09c46891122d6fa660b478dc4ec1149d2d7f7a6d
AUGUSTINAS VAINIUS <augustinas.vainius@gmail.com>
2019-04-30 21:28:40 +0300

    data collection schemas added & data from public API received

---
+++ backend/usersCollection.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const usersDataSchema = new Schema(
+    {
+        "_id": Number,
+        "userName": String,
+        "userInfo": String,
+        "products": [{
+            "productId": Number,
+            "productName": String, 
+            "description": String, 
+            "creationDate": Date, 
+            "modificationDate": Date, 
+            "expirydate": Date, 
+            "category": String, 
+            "priority": Number
+        }]
+    },
+    { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("usersCollection", usersDataSchema);
